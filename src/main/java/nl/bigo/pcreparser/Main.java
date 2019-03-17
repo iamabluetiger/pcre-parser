747537686827cf578eb928eb3484b6690d0930c4
ten <ten@sentbe.com>
2019-03-17 13:52:12 +0900

    Read file to parse the string

--- src/main/java/nl/bigo/pcreparser/Main.java
+++ src/main/java/nl/bigo/pcreparser/Main.java
@@ -1,15 +1,25 @@
 package nl.bigo.pcreparser;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 /**
  * A small demo class that demonstrates how to use the
  * generated parser classes.
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-
-        String regex = "((.)\\1+ (?<YEAR>(?:19|20)\\d{2})) [^]-x]";
-
-        System.out.println(new Builder.Tree(regex).toStringASCII());
+  public static void main(String[] args) throws Exception {
+    try {
+      File file = new File("/Users/ten/private-repo/pcre-parser/allrules-test.txt");
+      Scanner scan = new Scanner(file);
+      while(scan.hasNextLine()){
+        System.out.println(new Builder.Tree(scan.nextLine()).toStringASCII());
+      }
+    } catch (FileNotFoundException e) {
+      // TODO: handle exception
     }
+  }
 }
+
